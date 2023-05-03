da106221b61b1b7d53ead70789d5931c8412e00f
Dmitrii_Stashko <1028047@mail.ru>
2023-05-03 21:16:32 +1000

    Изменил решение задачи Obratka_spisok/Obratka_spisok.java

--- sem4/DZ/Obratka_spisok/Obratka_spisok.java
+++ sem4/DZ/Obratka_spisok/Obratka_spisok.java
@@ -8,6 +8,7 @@ package sem4.DZ.Obratka_spisok;
 // four
 // five
 
+import java.util.*;
 import java.util.LinkedList;
 public class Obratka_spisok {
     public static void main(String[] args) {
@@ -18,12 +19,24 @@ public class Obratka_spisok {
         numb.add("three");
         numb.add("four");
         numb.add("five");
-        System.out.println(numb);
+        System.out.println("\nSource List: " + numb);
+        reverseList(numb);
+    }
+    static void reverseList(LinkedList<String> numb) {
 
+        // первый способ:
         LinkedList<String> new_numb = new LinkedList<String>();
         for(String n : numb) {
             new_numb.addFirst(n);
         }
-        System.out.println(new_numb);
-    }   
+        System.out.println("First way: " + new_numb);
+
+        // или:
+        LinkedList<String> new_numb2 = new LinkedList<String>();
+        Iterator<String> col = numb.iterator();
+        while (col.hasNext()) {
+            new_numb2.addFirst(col.next());
+        }
+        System.out.println("Second way: " + new_numb + "\n");
+    }  
 }
