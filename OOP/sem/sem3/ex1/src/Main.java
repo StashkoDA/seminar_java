61f1259af0c2b9f7f60ed34932d26f171de05b08
Dmitrii_Stashko <1028047@mail.ru>
2023-12-01 10:19:45 +1000

    Создал ex1

---
+++ OOP/sem/sem3/ex1/src/Main.java
@@ -0,0 +1,17 @@
+//Реализуйте метод, принимающий в качестве аргумента целочисленный массив.
+//Если длина массива меньше некоторого заданного минимума,
+//метод возвращает -1, в качестве кода ошибки, иначе - длину массива.
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        System.out.println(getLength(arr, 2));
+    }
+
+    public static int getLength(int[] array, int minLength) {
+        if (array.length < minLength) {
+            return -1;
+        }
+        return array.length;
+    }
+}
