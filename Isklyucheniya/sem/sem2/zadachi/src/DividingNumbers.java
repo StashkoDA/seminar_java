d35028bcdf7ba18e495469a80f70f56c0f35616f
Dmitrii_Stashko <1028047@mail.ru>
2023-12-15 09:59:16 +1000

    Корректировка

--- Isklyucheniya/sem/sem2/zadachi/src/DividingNumbers.java
+++ Isklyucheniya/sem/sem2/zadachi/src/DividingNumbers.java
@@ -13,7 +13,7 @@ public class DividingNumbers {
 
         if (args.length == 0) {
             intArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-            d = 0; // По умолчанию используем 0, если аргумент не передан
+            d = 11; // По умолчанию используем 0, если аргумент не передан
         } else {
             intArray = Arrays.stream(args[0].split(" ")).mapToInt(Integer::parseInt).toArray();
             d = Integer.parseInt(args[1]); // Можно использовать значение по умолчанию или передать его как аргумент.
@@ -29,7 +29,10 @@ public class DividingNumbers {
             // Введите свое решение ниже
             try {
                 double res = intArray[8] / d;
-                System.out.println("intArray[8] / d = " + intArray[8] + " / " + d + " = " + res);
+                if (d != 0) {
+                    System.out.println("intArray[8] / d = " + intArray[8] + " / " + d + " = " + (double)intArray[8] / d);
+                    return (double)intArray[8] / d;
+                }
                 return res;
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("It's not possible to evaluate the expression - intArray[8] / d as there is no 8th element in the given array.");
