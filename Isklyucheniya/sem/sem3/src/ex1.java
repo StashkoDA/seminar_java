f4b658bc3e5422a65197d32e7b0cd1fb677ceaf2
Dmitrii_Stashko <1028047@mail.ru>
2023-12-19 18:02:57 +1000

    Добавил решение задачи ex1

--- Isklyucheniya/sem/sem3/src/ex1.java
+++ Isklyucheniya/sem/sem3/src/ex1.java
@@ -3,5 +3,16 @@
 //Вызовите этот метод из main и обработайте в нем исключение, которое
 //вызвал метод doSomething().
 
+import java.io.IOException;
+
 public class ex1 {
+    public static void main(String[] args) {
+        try {
+            doSomething();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void doSomething() throws IOException {}
 }
