74ab4660f93b4296948e338c96d4b4ef8572afda
Dmitrii_Stashko <1028047@mail.ru>
2023-12-27 10:50:31 +1000

    решаю

--- Isklyucheniya/sem/sem3/src/DZ_Notebook.java
+++ Isklyucheniya/sem/sem3/src/DZ_Notebook.java
@@ -47,25 +47,25 @@ public class DZ_Notebook {
             if (els.length == 6) {
                 System.out.println("Введено нужное количество параметров: " + els.length);
             } else if (els.length < 6) {
-                System.out.println("Введено недостаточное количество параметров: меньше " + els.length);
-                String el = els[5];
+                System.out.println("Введено недостаточное количество параметров: меньше 6");
+                String e = els[5];
             } else {
-                System.out.println("Введено избыточное количество параметров: больше " + els.length);
-                String el = els[els.length];
+                System.out.println("Введено избыточное количество параметров: больше 6");
+                String e = els[els.length];
             }
         }catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Повторите попытку.");
             }
-
     }
 
-        public static String[] errorOptions(String data)  {
+    public static String[] errorOptions(String data)  {
         String[] els = data.split(" ");
         String[] res = new String[6];
         SimpleDateFormat ft = new SimpleDateFormat ("dd.MM.yyyy");
 
         Date etalonMin = new Date(1900,01,01);
         Date etalonMax = new Date(2023,12,01);
+
         int count = 0;
 
         for (String el: els) {
@@ -79,26 +79,25 @@ public class DZ_Notebook {
 
 
             try {
-                String[] elData = el.split("."); // Преобразуем дату в в массив строк для анализа
-                System.out.println(elData.length); // ПОЧЕМУ-Т0 СИЧТАЕТ ДЛИНУ = 0
-                Date elPars1 = ft.parse(el);
-                System.out.println(elPars1);
-                System.out.println(elPars1.getYear());
+                String[] elData = el.split("."); // Преобразуем дату в массив строк для анализа
+
                 if (el.matches("\\d{12}")) { // Проверяем номер телефона:
                     parseInt(el);
                     res[4] = el;
+                    break;
                 } else if (el.length() != 11 && el.matches("\\d*")){
                     parseInt(el);
                     res[4] = el;
                     System.out.println("Номер телефона возможно введён с ошибкой.");
+                    break;
                 }
 
-                else if (elData.length == 3){
+                else if (el.matches("\\d{1,2}\\.\\d{1,2}\\.\\d{4}")){
                     Date elPars = ft.parse(el);
                     System.out.println(elPars);
                     if
-                    ((1900 + elPars.getYear()) >= 1900 && (1900 + elPars.getYear()) <= 2023)
-                    //(elPars.compareTo(etalonMin) > 0 && elPars.compareTo(etalonMax) < 0)
+                    //((1900 + elPars.getYear()) >= 1900 && (1900 + elPars.getYear()) <= 2023)
+                    (elPars.compareTo(etalonMin) > 0 && elPars.compareTo(etalonMax) < 0)
                     { // сравниваем дату с разрешённым диапазоном
                         res[3] = el;
                     } else {
